d20a2862ca0ee55ec25f2b2d6de7967e02e19b5a
miroslav351 <miroslav351@yahoo.com>
2018-11-12 10:53:10 +0000

    Fixed GROMACS path search

--- const.py
+++ const.py
@@ -7,6 +7,7 @@ import sys as _sys
 HOMEDIR = _os.path.dirname(_os.path.abspath(__file__))
 _sys.path.append("%s/shared" % HOMEDIR)
 OS = _platform.system()
+GROMACSEXE = None
 if OS == "Linux":
     if not "GROMACSHOME" in _os.environ:
         if any(sys in _platform.linux_distribution()[0].lower() for sys in ["red hat", "centos"]):
@@ -14,26 +15,24 @@ if OS == "Linux":
                                   shell=True)
             out, err = p.communicate()
             if err:
-                print("Module 'gromacs' not found on a Red Hat / CentOS system. Continuing without modules...")
+                p = _subprocess.Popen(["which gmx"], stdout=_subprocess.PIPE, stderr=_subprocess.PIPE, shell=True)
+                out, err = p.communicate()
+                if err:
+                    p = _subprocess.Popen(["which gmx_mpi"], stdout=_subprocess.PIPE, stderr=_subprocess.PIPE, shell=True)
+                    out, err = p.communicate()
+                    if err:
+                        print("GROMACS cannot be found on this system. This will affect some functionality.")
+                    else:
+                        _os.environ['GROMACSHOME'] = _re.match(r"([\S]*)/bin/gmx", out.decode()).group(1)
+                        GROMACSEXE = _os.environ['GROMACSHOME'] + "bin/gmx_mpi"
+                else:
+                    _os.environ['GROMACSHOME'] = _re.match(r"([\S]*)/bin/gmx", out.decode()).group(1)
             if out:
                 GROMACSHOME = _re.match(r"([\S]*)/bin/gmx", out.decode()).group(1)
                 _os.environ["GROMACSHOME"] = GROMACSHOME
-        else:
-            p = _subprocess.Popen(["which gmx"], stdout=_subprocess.PIPE, stderr=_subprocess.PIPE, shell=True)
-            out, err = p.communicate()
-            if err:
-                p = _subprocess.Popen(["which gmx_mpi"], stdout=_subprocess.PIPE, stderr=_subprocess.PIPE, shell=True)
-                out, err = p.communicate()
-                if err:
-                    print("GROMACS cannot be found on this system. This will affect some functionality.")
-                else:
-                    _os.environ['GROMACSHOME'] = _re.match(r"([\S]*)/bin/gmx", out.decode()).group(1)
-            else:
-                _os.environ['GROMACSHOME'] = _re.match(r"([\S]*)/bin/gmx", out.decode()).group(1)
-if "GROMACSHOME" in _os.environ:
+
+if "GROMACSHOME" in _os.environ and GROMACSEXE is None:
     GROMACSEXE = _os.environ["GROMACSHOME"] + "/bin/gmx"
-else:
-    GROMACSEXE = None
 PDB2PQRDIR = "%s/shared/pdb2pqr-%s" % (HOMEDIR, OS)
 assert _os.path.isdir(PDB2PQRDIR), "Cannot find pdb2pqr binary for the current operating system"
 _os.environ["SIRE_SILENT_PHONEHOME"] = "1"
