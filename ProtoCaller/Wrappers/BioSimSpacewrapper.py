e072de972bc864c47d7936e467a40ac88238fb99
miroslav351 <M.Suruzhon@soton.ac.uk>
2018-11-23 15:11:57 +0000

    Fixed GROMACS topology generation for replicas when neutralising

--- ProtoCaller/Wrappers/BioSimSpacewrapper.py
+++ ProtoCaller/Wrappers/BioSimSpacewrapper.py
@@ -1,5 +1,6 @@
 import BioSimSpace as _BSS
 import Sire.MM as _SireMM
+import Sire.Mol as _SireMol
 
 def rescaleSystemParams(system, scale, includelist=None, excludelist=None, neutralise=True):
     if excludelist is None and includelist is None:
@@ -7,12 +8,9 @@ def rescaleSystemParams(system, scale, includelist=None, excludelist=None, neutr
     elif excludelist is not None and includelist is not None:
         raise ValueError("Only one of includelist and excludelist can be set")
 
-    if scale == 1:
-        return system
-
     if neutralise:
         if excludelist is not None:
-            total_charge = round(sum([mol.charge().value() for mol in system.getMolecules()
+            total_charge = round(sum([mol.charge().magnitude() for mol in system.getMolecules()
                                       if mol._sire_molecule.name().value() not in excludelist]))
         elif includelist is not None:
             total_charge = round(sum([mol.charge().magnitude() for mol in system.getMolecules()
@@ -36,16 +34,17 @@ def rescaleSystemParams(system, scale, includelist=None, excludelist=None, neutr
 
         mol_edit = mol._sire_molecule.edit()
 
-        for prop in ["dihedral", "dihedral0", "dihedral1", "improper", "improper0", "improper1"]:
-            try:
-                potentials = mol_edit.property(prop).potentials()
-                prop_new = _SireMM.FourAtomFunctions(mol_edit.info())
-                for potential in potentials:
-                    prop_new.set(potential.atom0(), potential.atom1(), potential.atom2(), potential.atom3(),
-                                 potential.function() * scale)
-                mol_edit = mol_edit.setProperty(prop, prop_new).molecule()
-            except:
-                continue
+        if scale != 1:
+            for prop in ["dihedral", "dihedral0", "dihedral1", "improper", "improper0", "improper1"]:
+                try:
+                    potentials = mol_edit.property(prop).potentials()
+                    prop_new = _SireMM.FourAtomFunctions(mol_edit.info())
+                    for potential in potentials:
+                        prop_new.set(potential.atom0(), potential.atom1(), potential.atom2(), potential.atom3(),
+                                     potential.function() * scale)
+                    mol_edit = mol_edit.setProperty(prop, prop_new).molecule()
+                except:
+                    continue
 
         for atom in mol_edit.atoms():
             idx = atom.index()
@@ -57,21 +56,27 @@ def rescaleSystemParams(system, scale, includelist=None, excludelist=None, neutr
                 except:
                     continue
 
-            for prop in ["LJ", "LJ0", "LJ1"]:
-                try:
-                    LJ = atom.property(prop)
-                    LJ = LJ.fromSigmaAndEpsilon(LJ.sigma(), LJ.epsilon() * scale)
-                    mol_edit = mol_edit.atom(idx).setProperty(prop, LJ).molecule()
-                except:
-                    continue
+            if scale != 1:
+                for prop in ["LJ", "LJ0", "LJ1"]:
+                    try:
+                        LJ = atom.property(prop)
+                        LJ = LJ.fromSigmaAndEpsilon(LJ.sigma(), LJ.epsilon() * scale)
+                        mol_edit = mol_edit.atom(idx).setProperty(prop, LJ).molecule()
+                    except:
+                        continue
 
-            for prop in ["charge", "charge0", "charge1"]:
-                try:
-                    charge = atom.property(prop)
-                    charge = charge * scale
-                    mol_edit = mol_edit.atom(idx).setProperty(prop, charge).molecule()
-                except:
-                    continue
+                for prop in ["charge", "charge0", "charge1"]:
+                    try:
+                        charge = atom.property(prop)
+                        charge = charge * scale
+                        mol_edit = mol_edit.atom(idx).setProperty(prop, charge).molecule()
+                    except:
+                        continue
+
+        if mol._sire_molecule.name().value().lower() in ["na", "cl"]:
+            resname = mol_edit.residue().name()
+            resname_new = _SireMol.ResName(resname.value() + "_")
+            mol_edit = mol_edit.residue(resname).rename(resname_new)
 
         mol._sire_molecule = mol_edit.commit()
         mols_mod += [mol]
