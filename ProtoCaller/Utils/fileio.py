e6f02e46c93dc528af03f87866eb8c7f35edd303
Miroslav <miroslav351@yahoo.com>
2018-12-20 07:49:46 +0000

    Fixed some bugs regarding temporary folders

--- ProtoCaller/Utils/fileio.py
+++ ProtoCaller/Utils/fileio.py
@@ -1,4 +1,5 @@
 #makes the generation of subdirectories a bit easier, more readable and less prone to errors
+import atexit as _atexit
 import os as _os
 import shutil as _shutil
 
@@ -10,19 +11,25 @@ class Subdir():
         self.temp = temp
     def __enter__(self):
         self.workdirname = _os.getcwd()
-        if(self.overwrite and _os.path.exists(self.dirname)):
+        if self.overwrite and _os.path.exists(self.dirname):
             _shutil.rmtree("%s/%s" % (self.workdirname, self.dirname))
-        if(self.overwrite and self.copydirname and _os.path.exists(self.copydirname)):
+        if self.overwrite and self.copydirname and _os.path.exists(self.copydirname):
             _shutil.copytree("%s/%s" % (self.workdirname, self.copydirname), "%s/%s" % (self.workdirname, self.dirname))
-        elif(not _os.path.exists(self.dirname)):
+        elif not _os.path.exists(self.dirname):
             _os.makedirs(self.dirname)
         _os.chdir(self.dirname)
         self.path = "%s/%s" % (self.workdirname, self.dirname)
         return self
     def __exit__(self, exc_type, exc_value, traceback):
         _os.chdir(self.workdirname)
+        #removes temporary directory at the end of execution
         if(self.temp):
-            _shutil.rmtree(self.path)
+            def exit():
+                try:
+                    _shutil.rmtree(self.path)
+                except:
+                    pass
+            _atexit.register(exit)
 
 class Dir(Subdir):
     def __init__(self, dirname, *args, **kwargs):
@@ -31,7 +38,7 @@ class Dir(Subdir):
         else:
             self.workdirname = _os.getcwd()
         self.initialdirname = _os.getcwd()
-        super().__init__(_os.path.basename(dirname), *args, *kwargs)
+        super().__init__(_os.path.basename(dirname), *args, **kwargs)
 
     def __enter__(self):
         _os.chdir(self.workdirname)
