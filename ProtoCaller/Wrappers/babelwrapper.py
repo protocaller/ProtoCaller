f1e5cfb72d278d5e21b5815de652a0aeff606df9
Miroslav Suruzhon <miroslav351@yahoo.com>
2019-02-01 16:06:04 +0000

    Minor fix in how babelwrapper handles file names

--- ProtoCaller/Wrappers/babelwrapper.py
+++ ProtoCaller/Wrappers/babelwrapper.py
@@ -6,11 +6,11 @@ def babelTransform(input_filename, output_extension="mol2", pH=7.0):
     if input_filename is None:
         return None
 
-    input_filebase, input_extension = input_filename.split(".")
+    input_filebase, input_extension = _os.path.splitext(input_filename)[0], input_filename.split(".")[-1]
     output_filename = _os.path.abspath(_os.path.basename(input_filebase) + "." + output_extension)
     command = "obabel -i {0} \"{1}.{0}\" -o \"{2}\" -O {3}".format(input_extension, input_filebase, output_extension,
                                                                    output_filename)
     if pH is not None: command += " -p {}".format(pH)
     _runexternal.runExternal(command, procname="OpenBabel")
 
-    return output_filename
+    return _os.path.abspath(output_filename)
