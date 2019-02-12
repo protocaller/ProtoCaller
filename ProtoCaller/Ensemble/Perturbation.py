6f8517d67e5090df5185e17417a011e87ce686f2
Miroslav Suruzhon <miroslav351@yahoo.com>
2019-02-12 17:11:49 +0000

    Created PerturbationList class and modified the Perturbation class

--- ProtoCaller/Ensemble/Perturbation.py
+++ ProtoCaller/Ensemble/Perturbation.py
@@ -6,24 +6,24 @@ import BioSimSpace as _BSS
 
 from .Ligand import Ligand as _Ligand
 import ProtoCaller.Wrappers.rdkitwrapper as _rdkit
+import ProtoCaller.Utils.stdio as _stdio
 
 
 class Perturbation:
     _counter = 1
 
     def __init__(self, ligand1, ligand2, name=None):
-        try:
-            assert isinstance(ligand1, _Ligand) and isinstance(ligand2, _Ligand)
-        except AssertionError:
+        if not isinstance(ligand1, _Ligand) and not isinstance(ligand2, _Ligand):
             raise TypeError("Input ligands need to be of type Ligand")
 
-        self._ligand1 = ligand1
-        self._ligand2 = ligand2
-        self._ligand1_molecule = None
-        self._ligand2_molecule = None
-        self._ligand1_coords = None
-        self._ligand2_coords = None
-        self._morph = None
+        self.ligand1 = ligand1
+        self.ligand2 = ligand2
+        self.current_ref = None
+        self._ligand1_molecule = {}
+        self._ligand2_molecule = {}
+        self._ligand1_coords = {}
+        self._ligand2_coords = {}
+        self._morph = {}
         self.name = name
 
     @property
@@ -36,17 +36,32 @@ class Perturbation:
             self._name = "Pair %d" % self._counter
             Perturbation._counter += 1
         elif val is None:
-            self._name = "{}~{}".format(self._ligand1.name, self._ligand2.name)
+            self._name = "{}~{}".format(self.ligand1.name, self.ligand2.name)
         else:
             self._name = val
 
+    def get_parametrised_files1(self, ref):
+        topol = None if not self.ligand1.parametrised else self.ligand1.parametrised_files[0]
+        if self._ligand1_coords is not None:
+            coords = self._ligand1_coords[ref]
+        else:
+            coords = None if not self.ligand1.parametrised else self.ligand1.parametrised_files[1]
+
+        if topol is None or coords is None:
+            return None
+        else:
+            return [topol, coords]
+
     @property
     def parametrised_files1(self):
-        topol = None if not self._ligand1.parametrised else self._ligand1.parametrised_files[0]
-        if self._ligand1_coords is not None:
-            coords = self._ligand1_coords
+        return self.get_parametrised_files1(ref=self.current_ref)
+
+    def get_parametrised_files2(self, ref):
+        topol = None if not self.ligand2.parametrised else self.ligand2.parametrised_files[0]
+        if self._ligand2_coords is not None:
+            coords = self._ligand2_coords[ref]
         else:
-            coords = None if not self._ligand1.parametrised else self._ligand1.parametrised_files[1]
+            coords = None if not self.ligand2.parametrised else self.ligand2.parametrised_files[1]
 
         if topol is None or coords is None:
             return None
@@ -55,50 +70,61 @@ class Perturbation:
 
     @property
     def parametrised_files2(self):
-        topol = None if not self._ligand2.parametrised else self._ligand2.parametrised_files[0]
-        if self._ligand2_coords is not None:
-            coords = self._ligand2_coords
-        else:
-            coords = None if not self._ligand2.parametrised else self._ligand2.parametrised_files[1]
+        return self.get_parametrised_files2(ref=self.current_ref)
 
-        if topol is None or coords is None:
-            return None
-        else:
-            return [topol, coords]
+    def get_morph(self, ref):
+        return self._morph[ref]
 
     @property
     def morph(self):
-        return self._morph
+        return self._morph[self.current_ref]
 
-    def alignToReference(self, ref, output_filename=None, **kwargs):
+    def isAlignedTo(self, ref):
+        return ref in self._morph.keys()
+
+    @property
+    def isAligned(self):
+        return self.isAlignedTo(self.current_ref)
+
+    def alignToReference(self, ref, output_filename=None, realign=False, **kwargs):
+        self.current_ref = ref
+        if ref in self._ligand1_molecule.keys() and ref in self._ligand1_coords.keys() and not realign:
+            print("Morph %s is already aligned to a reference" % self.name)
+            return
+
         if output_filename is None:
-            if not self._ligand1.parametrised:
-                self._ligand1.parametrise()
-            output_filename = "{}_aligned_{}.{}".format(self._ligand1.name, self.name,
-                                                        self._ligand1.parametrised_files[1].split(".")[-1])
+            _stdio.stdout_stderr()(self.ligand1.parametrise)(reparametrise=False)
+            output_filename = "{}_aligned_{}.{}".format(self.ligand1.name, self.name,
+                                                        self.ligand1.parametrised_files[1].split(".")[-1])
         if "always_maximum" not in kwargs.keys(): kwargs["always_maximum"] = False
 
-        ref_temp = _copy.deepcopy(ref.molecule)
-        lig_temp = _copy.deepcopy(self._ligand1.molecule)
-        self._ligand1_molecule, mcs = _rdkit.alignTwoMolecules(ref_temp, lig_temp, **kwargs)
-        self._ligand1_coords = _os.path.abspath(_rdkit.saveFromRdkit(self._ligand1_molecule, output_filename))
+        lig_temp = _copy.deepcopy(self.ligand1.molecule)
+        self._ligand1_molecule[ref], mcs = _rdkit.alignTwoMolecules(ref.molecule, lig_temp, **kwargs)
+        self._ligand1_coords[ref] = _os.path.abspath(_rdkit.saveFromRdkit(self._ligand1_molecule[ref], output_filename))
 
         return mcs
 
-    def alignToEachOther(self, output_filename=None, **kwargs):
+    def alignToEachOther(self, output_filename=None, realign=False, **kwargs):
+        if self.current_ref in self._morph.keys() and not realign:
+            print("Morph %s is already aligned to a reference" % self.name)
+            return
+
         if output_filename is None:
-            if not self._ligand1.parametrised:
-                self._ligand1.parametrise()
-            if not self._ligand2.parametrised:
-                self._ligand2.parametrise()
-            output_filename = "{}_aligned_{}.{}".format(self._ligand2.name, self.name,
-                                                        self._ligand2.parametrised_files[1].split(".")[-1])
+            _stdio.stdout_stderr()(self.ligand1.parametrise)(reparametrise=False)
+            _stdio.stdout_stderr()(self.ligand2.parametrise)(reparametrise=False)
+            output_filename = "{}_aligned_{}.{}".format(self.ligand2.name, self.name,
+                                                        self.ligand2.parametrised_files[1].split(".")[-1])
         if "always_maximum" not in kwargs.keys(): kwargs["always_maximum"] = True
 
-        lig1_temp = self._ligand1_molecule if self._ligand1_molecule is not None else self._ligand1.molecule
-        lig2_temp = _copy.deepcopy(self._ligand2.molecule)
-        self._ligand2_molecule, mcs = _rdkit.alignTwoMolecules(lig1_temp, lig2_temp, **kwargs)
-        self._ligand2_coords = _os.path.abspath(_rdkit.saveFromRdkit(self._ligand2_molecule, output_filename))
+        if self.current_ref is None:
+            self._ligand1_molecule[None] = self.ligand1.molecule
+            self._ligand1_coords[None] = self.ligand1.parametrised_files[1]
+
+        lig1_temp = self._ligand1_molecule[self.current_ref]
+        lig2_temp = _copy.deepcopy(self.ligand2.molecule)
+        self._ligand2_molecule[self.current_ref], mcs = _rdkit.alignTwoMolecules(lig1_temp, lig2_temp, **kwargs)
+        self._ligand2_coords[self.current_ref] = _os.path.abspath(
+            _rdkit.saveFromRdkit(self._ligand2_molecule[self.current_ref], output_filename))
 
         # load the shifted ligands into BioSimSpace
         ligand1_BSS = _BSS.IO.readMolecules(self.parametrised_files1).getMolecules()[0]
@@ -112,9 +138,9 @@ class Perturbation:
             mapping[indices_1[idx1]] = indices_2[idx2]
 
         # finally create morph
-        self._morph = _BSS.Align.merge(ligand1_BSS, ligand2_BSS, mapping=mapping)
+        self._morph[self.current_ref] = _BSS.Align.merge(ligand1_BSS, ligand2_BSS, mapping=mapping)
 
-        return self._morph, mcs
+        return self._morph[self.current_ref], mcs
 
     # default alignment method wrapping the two alignment stages
     def alignAndCreateMorph(self, ref):
---
+++ ProtoCaller/Ensemble/PerturbationList.py
@@ -0,0 +1,17 @@
+from .Ligand import Ligand as _Ligand
+from .Perturbation import Perturbation as _Perturbation
+from ProtoCaller.Utils.ConditionalList import ConditionalList as _ConditionalList
+
+
+class PerturbationList(_ConditionalList):
+    def __init__(self, perturbations):
+        _ConditionalList.__init__(self, perturbations, transformfunc=self._transformMorph)
+
+    @staticmethod
+    def _transformMorph(morph):
+        if isinstance(morph, _Perturbation):
+            return morph
+        elif isinstance(morph, list):
+            if len(morph) != 2:
+                raise ValueError("Need a list of 2 Ligands for a perturbation")
+            return _Perturbation(*[x if isinstance(x, _Ligand) else _Ligand(x) for x in morph])
