74b8e1df38e2a69fff93d26ce51f548b0e91fbd7
Miroslav Suruzhon <miroslav351@yahoo.com>
2019-02-12 17:10:20 +0000

    Added extra functionality to ConditionalList

--- ProtoCaller/Utils/ConditionalList.py
+++ ProtoCaller/Utils/ConditionalList.py
@@ -1,6 +1,7 @@
 class ConditionalList(list):
     # a class that conditionally accepts new elements based on user-defined functions
-    def __init__(self, input_list, *checkfuncs):
+    def __init__(self, input_list, *checkfuncs, transformfunc=None):
+        if checkfuncs is None: checkfuncs = []
         if not isinstance(input_list, list):
             input_list = [input_list]
         for checkfunc in checkfuncs:
@@ -8,11 +9,14 @@ class ConditionalList(list):
                 raise TypeError("Need to pass a callable function as a parameter")
             for item in input_list:
                 checkfunc(item)
+        if transformfunc is not None:
+            input_list = [transformfunc(x) for x in input_list]
         list.__init__(self, input_list)
         self._checkfuncs = list(checkfuncs)
+        self._transformfunc = transformfunc
 
         for item in ["__add__", "__iadd__", "append", "extend", "insert", "remove"]:
-            setattr(ConditionalList, item, self._check(getattr(list, item)))
+            setattr(self, item, self._check(getattr(self, item)))
 
     def _check(self, listfunc):
         def decorated(*args):
@@ -23,5 +27,8 @@ class ConditionalList(list):
                 for checkfunc in self._checkfuncs:
                     for item in items:
                         checkfunc(item)
+                if self._transformfunc is not None:
+                    items = [self._transformfunc(x) for x in items]
+                    return listfunc(*args[:-1], items)
             return listfunc(*args)
         return decorated
