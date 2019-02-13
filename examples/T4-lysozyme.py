a957e216b147d01615bf4c7b61b86575017d004b
Miroslav Suruzhon <miroslav351@yahoo.com>
2019-02-13 10:03:15 +0000

    Fixed a small omission in the T4-lysozyme example

--- examples/T4-lysozyme.py
+++ examples/T4-lysozyme.py
@@ -1,12 +1,11 @@
 import os, sys
 # TODO: need to append the folder where ProtoCaller is installed. This is going to be fixed in a later version
-sys.path.append("/home/ms2m18/")
+sys.path.append(os.path.expanduser("~/ProtoCaller"))
 # add an alternative default version for GROMACS. Otherwise, use bash default
-# os.environ["GROMACSHOME"] = "/home/ms2m18/gromacs-2018.4"
+# os.environ["GROMACSHOME"] = os.path.expanduser("~/gromacs-2018.4")
 
 from ProtoCaller.Utils.fileio import Dir
-import ProtoCaller.Ensemble as Ensemble
-from ProtoCaller.Ensemble import Ligand, Protein
+from ProtoCaller.Ensemble import Ligand, Protein, Ensemble
 
 with Dir("T4-lysozyme", overwrite=True):
     # create a protein from its PDB code and the residue number of the ligand we are going to use for mapping
@@ -18,8 +17,8 @@ with Dir("T4-lysozyme", overwrite=True):
     morphs = [[benzol, o_xylene], [o_xylene, benzol]]
 
     # create a system from the protein and the morphs and set up some default settings regarding system preparation
-    system = Ensemble.Ensemble("GROMACS", protein=protein, morphs=morphs, box_length_complex=7, ligand_ff="gaff2",
-                               workdir=protein.workdir.path)
+    system = Ensemble("GROMACS", protein=protein, morphs=morphs, box_length_complex=7, ligand_ff="gaff2",
+                      workdir=protein.workdir.path)
     # only keep the reference ligand and keep all crystallographic waters
     system.protein.filter(ligands=None, waters="all")
     # protonate the protein using PDB2PQR and add missing atoms / residues if needed
