ee7ef39f45647891ebea104beabd87bb265de583
Miroslav Suruzhon <miroslav351@yahoo.com>
2019-02-15 14:29:06 +0000

    Added chirality handling in MCS. However, more additions might be needed for proper functionality

--- ProtoCaller/Wrappers/rdkitwrapper.py
+++ ProtoCaller/Wrappers/rdkitwrapper.py
@@ -1,3 +1,5 @@
+from collections import Counter as _Counter
+import copy as _copy
 import os as _os
 
 from rdkit import Chem as _Chem
@@ -98,7 +100,7 @@ def translateMolecule(mol, vector):
     return mol
 
 
-def getMCSMap(ref, mol, match="any", always_maximum=True):
+def getMCSMap(ref, mol, match="any", always_maximum=True, matchChiralTag=True, **kwargs):
     """
     :param mol: molecule to be aligned
     :param ref: reference molecule
@@ -106,11 +108,78 @@ def getMCSMap(ref, mol, match="any", always_maximum=True):
                            topology mapping. False is good for overall structure alignment. Note that in some more
                            complex molecules there might be an MCS that satisfies our criteria and is bigger. Here we
                            rely on there being one obvious MCS - something that should be the case in free energy
-                           calculations anyway
+                           calculations anyway. Note that this option does not apply to chiral symmetric molecules
+    :param matchChiralTag: take care of stereochemistry. Here we don't use rdkit's default behaviour because it is not
+                           good for our purposes. We instead opt for a maximum common substructure and then pruning all
+                           atoms that will not obey the stereochemistry. While this option does not work perfectly for
+                           symmetric chiral molecules, it should work rather well in the general case. Again, the
+                           results are best when the two molecules are similar in structure which should be the case
+                           in FE calculations
     :param match: "any" matches any pairs of atoms - useful for single-topology;
                   "elements" matches only the same atoms - useful for dual-topology
     :return: the aligned molecule
     """
+    def matchAndReturnMatches(*args, **kwargs):
+        mcs_string = _FMCS.FindMCS(*args, **kwargs).smartsString
+        mcs = _Chem.MolFromSmarts(mcs_string)
+        return mcs, list(zip(ref.GetSubstructMatch(mcs), mol.GetSubstructMatch(mcs)))
+
+    def transformIndices(indices_new, indices_to_delete):
+        # take into account the fact that indices change when atoms are deleted
+        return [x + sum(x >= y for y in sorted(indices_to_delete)) for x in sorted(indices_new)]
+
+    def fixIndicesToDelete(indices_to_delete, mcs, matches):
+        # accept a molecule and indices to delete and return the indices from the old molecule corresponding to the
+        # biggest fragment in the new one
+        mcs_edit = _Chem.EditableMol(_copy.deepcopy(mcs))
+        for index in reversed(indices_to_delete):
+            mcs_edit.RemoveAtom(index)
+        indices_new = max(_rdmolops.GetMolFrags(mcs_edit.GetMol(), asMols=False, sanitizeFrags=False), key=len)
+        return [x for i, x in enumerate(matches) if i in transformIndices(indices_new, indices_to_delete)]
+
+    def fixChiralIndices(indices, mcs, matches):
+        # delete all specified indices and create fragments recursively so that each one of them contains at least one
+        # of the indices
+        fragments = {frozenset(i for i in range(mcs.GetNumAtoms()))}
+        for index in indices:
+            fragments_new = set()
+            for fragment in fragments:
+                # every fragment is stored as a set of indices. Here we recreate this fragment from the MCS
+                # then we fragment this fragment further by deleting the index from it
+                if index not in fragment:
+                    fragments_new.add(fragment)
+                    continue
+                indices_to_delete = [x for x in range(mcs.GetNumAtoms()) if x == index or x not in fragment]
+                mcs_edit = _Chem.EditableMol(_copy.deepcopy(mcs))
+                for index_to_delete in reversed(indices_to_delete):
+                    mcs_edit.RemoveAtom(index_to_delete)
+                # the new fragments resulting from the fragment have new indices that need to be translated back
+                fragment_new = {frozenset(sorted(list(fragment))[i] for i in transformIndices(x, [index]) + [index])
+                                for x in _rdmolops.GetMolFrags(mcs_edit.GetMol(), asMols=False, sanitizeFrags=False)}
+                fragments_new |= fragment_new
+            fragments = fragments_new
+        return [x for i, x in enumerate(matches) if i in max(fragments, key=len)]
+
+    def fixMCS(mcs, matches):
+        # check whether there are any improperly assigned rings (e.g. 5-membered to 6-membered) and flag the atoms
+        # also check if there is any chirality mismatch and flag the atoms if this is the case
+        indices_to_delete, indices_chiral = [], []
+        for i_mcs, (i_ref, i_mol) in enumerate(matches):
+            rings_ref, rings_mol = getRings(i_ref, ref), getRings(i_mol, mol)
+            is_wrong_mapping = not isSublist(rings_ref, rings_mol) and not isSublist(rings_mol, rings_ref)
+            is_different_chirality = matchChiralTag and i_ref in chiral_ref.keys() and i_mol in chiral_mol.keys() and \
+                                     chiral_ref[i_ref] != chiral_mol[i_mol]
+            if is_wrong_mapping:
+                indices_to_delete += [i_mcs]
+            elif is_different_chirality:
+                indices_chiral += [i_mcs]
+        # delete invalid atoms
+        if indices_to_delete:
+            matches = fixIndicesToDelete(indices_to_delete, mcs, matches)
+        if indices_chiral:
+            matches = fixChiralIndices(indices_chiral, mcs, matches)
+        return matches
+
     match = match.strip().lower()
     if match == "any":
         comp_method = _FMCS.AtomCompare.CompareAny
@@ -119,35 +188,29 @@ def getMCSMap(ref, mol, match="any", always_maximum=True):
     else:
         raise ValueError("'match' arguments needs to be either 'any' or 'elements'")
 
-    # find loose MCS
-    mcs_string = _FMCS.FindMCS([ref, mol], atomCompare=comp_method, bondCompare=_FMCS.BondCompare.CompareAny,
-                               ringMatchesRingOnly=False, completeRingsOnly=False).smartsString
-    mcs = _Chem.MolFromSmarts(mcs_string)
-    matches = list(zip(ref.GetSubstructMatch(mcs), mol.GetSubstructMatch(mcs)))
+    kwargs = {
+        "atomCompare": comp_method,
+        "bondCompare": _FMCS.BondCompare.CompareAny,
+        "ringMatchesRingOnly": False,
+        **kwargs,
+    }
 
-    # check whether there are any improperly assigned rings (e.g. 5-membered to 6-membered) and flag the atoms
-    indices_to_delete = []
-    for i_mcs, (i_ref, i_mol) in enumerate(matches):
-        rings_ref, rings_mol = getRings(i_ref, ref), getRings(i_mol, mol)
-        if not set(rings_ref).issubset(rings_mol) and not set(rings_mol).issubset(rings_ref):
-            indices_to_delete += [i_mcs]
+    # get the chiral centres
+    chiral_ref = dict(_Chem.FindMolChiralCenters(ref))
+    chiral_mol = dict(_Chem.FindMolChiralCenters(mol))
 
-    # delete these atoms from the MCS and get the biggest fragment
-    if indices_to_delete:
-        mcs = _Chem.EditableMol(mcs)
-        for index in reversed(indices_to_delete):
-            mcs.RemoveAtom(index)
-
-        indices_new = max(_rdmolops.GetMolFrags(mcs.GetMol(), asMols=False, sanitizeFrags=False), key=len)
-        matches = [x for i, x in enumerate(matches) if i in indices_new]
+    # find loose MCS. matchChiralTag is always False because of bad combined functionality of completeRingsOnly False
+    # and matchChiralTag True. We try to fix any wrong results later on in the code.
+    mcs, matches = matchAndReturnMatches([ref, mol], completeRingsOnly=False, matchChiralTag=False, **kwargs)
+    matches = fixMCS(mcs, matches)
 
     # now get the strict mapping and discard the loose mapping if it doesn't perform better
     if always_maximum:
-        mcs_string_strict = _FMCS.FindMCS([ref, mol], atomCompare=comp_method, bondCompare=_FMCS.BondCompare.CompareAny,
-                                          ringMatchesRingOnly=False, completeRingsOnly=True).smartsString
-        mcs_strict = _Chem.MolFromSmarts(mcs_string_strict)
-        matches_strict = list(zip(ref.GetSubstructMatch(mcs_strict), mol.GetSubstructMatch(mcs_strict)))
-        matches = matches if len(matches) > len(matches_strict) else matches_strict
+        mcs, matches_strict = matchAndReturnMatches([ref, mol], completeRingsOnly=True, matchChiralTag=False, **kwargs)
+        if matchChiralTag:
+            matches_strict = fixMCS(mcs, matches_strict)
+        if len(matches) < len(matches_strict):
+            return matches_strict
 
     return matches
 
@@ -180,3 +243,11 @@ def alignTwoMolecules(ref, mol, n_min=-1, mcs=None, **kwargs):
 def getRings(atom_idx, molecule):
     rings = molecule.GetRingInfo().AtomRings()
     return sorted([len(ring) for ring in rings if atom_idx in ring])
+
+
+def isSublist(l1, l2):
+    c1, c2 = _Counter(l1), _Counter(l2)
+    for k, n in c1.items():
+        if n > c2[k]:
+            return False
+    return True
--- test/Wrappers/test_rdkitwrapper.py
+++ test/Wrappers/test_rdkitwrapper.py
@@ -9,4 +9,16 @@ def test_MCS():
 
     ref = openAsRdkit("C1CCC(CC1)CCCC2CCCC2")
     mol = openAsRdkit("C1CCC(CC1)C2OC(CC2)C3CCNC3")
-    assert len(getMCSMap(ref, mol)) == 14
+    assert len(getMCSMap(ref, mol, always_maximum=True)) == 14
+
+    # test some stereospecific mapping
+    ref = openAsRdkit("C1CC[C@@H](C2CCCC2)[C@@H](C3CCC3)C1")
+
+    mol = openAsRdkit("C1CC[C@@H](C2CCCC2)[C@H](C3CCC3)C1")
+    assert len(getMCSMap(ref, mol, match="any", always_maximum=True, matchChiralTag=True)) == 11
+    mol = openAsRdkit("C1CC[C@H](C2CCCC2)[C@@H](C3CCC3)C1")
+    assert len(getMCSMap(ref, mol, match="any", always_maximum=True, matchChiralTag=True)) == 10
+    mol = openAsRdkit("C1CC[C@H](C2CCCC2)[C@H](C3CCC3)C1")
+    assert len(getMCSMap(ref, mol, match="any", always_maximum=True, matchChiralTag=True)) == 6
+    mol = openAsRdkit("C1CC[C@@H](C2CCCC2)[C@@H](C3CCC3)C1")
+    assert len(getMCSMap(ref, mol, match="any", always_maximum=True, matchChiralTag=True)) == 15
