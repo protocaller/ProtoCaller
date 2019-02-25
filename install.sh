7a6f22f1f6ea0f9552ab0e85a186a80ff815a8d4
Miroslav Suruzhon <miroslav351@yahoo.com>
2019-02-25 10:39:20 +0000

    Fixed some issues with install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ "$OSTYPE" == "linux-gnu" ]; then
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
     FILE="biosimspace_devel_latest_linux.run"
-    if [ ! -f $FILE ]; then
+    if [[ ! -f $FILE ]]; then
         wget https://objectstorage.eu-frankfurt-1.oraclecloud.com/p/ZH4wscDHe59T28yVJtrMH8uqifI_ih0NL5IyqxXQjSo/n/chryswoods/b/biosimspace_releases/o/biosimspace_devel_latest_linux.run
     fi
-elif [ "$OSTYPE" == "darwin"* ] && [ ! -f biosimspace_devel_latest_os.run ]; then
+elif [[ "$OSTYPE" == "darwin"* ]]; then
     FILE = biosimspace_devel_latest_os.run
-    if [ ! -f $FILE ]; then
+    if [[ ! -f $FILE ]]; then
         wget https://objectstorage.eu-frankfurt-1.oraclecloud.com/p/whcwfvWfndjA4RxupM-4gsVsjcdR0w5I9aP1RJKPruQ/n/chryswoods/b/biosimspace_releases/o/biosimspace_devel_latest_osx.run
     fi
 else
@@ -19,4 +19,4 @@ chmod +x "$FILE"
 { echo ; } | "./$FILE" --nox11
 source activate ~/biosimspace.app/
 sleep 5
-python setup.py install
+python setup.py -q install
