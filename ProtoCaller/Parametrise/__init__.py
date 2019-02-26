0c578f2935e29ed6dc5d4c8b305f4096ddc05440
Miroslav Suruzhon <miroslav351@yahoo.com>
2019-02-26 11:14:31 +0000

    Added docstrings to Parametrise

--- ProtoCaller/Parametrise/__init__.py
+++ ProtoCaller/Parametrise/__init__.py
@@ -4,9 +4,22 @@ if _PC.BIOSIMSPACE:
     import BioSimSpace as _BSS
 
 import ProtoCaller.Wrappers.parmedwrapper as _pmdwrap
-from . import _amber
+from . import amber
+
 
 class Params:
+    """
+    A simple class which contains force field names different molecule types.
+
+    Parameters
+    ----------
+    protein_ff: str
+        Initialises the protein force field.
+    ligand_ff: str
+        Initialises the ligand force field.
+    water_ff: str
+        Initialises the water / simple ion force field.
+    """
     def __init__(self, protein_ff=_PC.AMBERDEFAULTPROTEINFF, ligand_ff=_PC.AMBERDEFAULTLIGANDFF,
                  water_ff=_PC.AMBERDEFAULTWATERFF):
         self.protein_ff = protein_ff
@@ -15,6 +28,7 @@ class Params:
 
     @property
     def protein_ff(self):
+        """The protein force field."""
         return self._protein_ff
 
     @protein_ff.setter
@@ -26,6 +40,7 @@ class Params:
 
     @property
     def ligand_ff(self):
+        """The ligand force field."""
         return self._ligand_ff
 
     @ligand_ff.setter
@@ -37,6 +52,7 @@ class Params:
 
     @property
     def water_ff(self):
+        """The water / simple ion force field."""
         return self._water_ff
 
     @water_ff.setter
@@ -46,30 +62,94 @@ class Params:
             raise ValueError("Value %s not supported. Supported values: " % val, _PC.WATERFFS)
         self._water_ff = val
 
+
 def parametriseAndLoadPmd(params, *args, **kwargs):
+    """
+    Calls parametriseFile and loads the files into ParmEd.
+
+    Parameters
+    ----------
+    params : ProtoCaller.Parametrise.Params
+        Force field parameters.
+    args
+        Positional arguments to be passed to parametriseFile.
+    kwargs
+        Keyword arguments to be passed to parametriseFile.
+
+    Returns
+    -------
+    system : parmed.structure.Structure
+        The loaded parametrised files as a ParmEd Structure.
+    """
     files = parametriseFile(params, *args, **kwargs)
     return _pmdwrap.openFilesAsParmed(files)
 
+
 def parametriseAndLoadBSS(params, *args, **kwargs):
+    """
+    Calls parametriseFile and loads the files into BioSimSpace.
+
+    Parameters
+    ----------
+    params : ProtoCaller.Parametrise.Params
+        Force field parameters.
+    args
+        Positional arguments to be passed to parametriseFile.
+    kwargs
+        Keyword arguments to be passed to parametriseFile.
+
+    Returns
+    -------
+    system : BioSimSpace.system
+        The loaded parametrised files as a BioSimSpace System.
+    """
     if _PC.BIOSIMSPACE:
         files = parametriseFile(params, *args, **kwargs)
         return _BSS.IO.readMolecules(files)
     else:
         raise ImportError("BioSimSpace module cannot be imported")
 
+
 def parametriseFile(params, filename, molecule_type, fix_charge=True, id=None, *args, **kwargs):
+    """
+    Parametrises an input file according to a force field.
+
+    Parameters
+    ----------
+    params : ProtoCaller.Parametrise.Params
+        Force field parameters.
+    filename : str
+        Name of the input file.
+    molecule_type : str
+        The type of the molecule. One of: "protein", "ligand", "cofactor", "water", "simple_anion", "complex_anion",
+        "simple_cation", "complex_cation".
+    fix_charge : bool
+        Whether to enforce integer net charge.
+    id : str
+        The name of the molecule. Default: equal to molecule_type.
+    args
+        Positional arguments to be passed to the relevant wrapper.
+    kwargs
+        Keyword arguments to be passed to the relevant wrapper.
+
+    Returns
+    -------
+    files : [str]
+        The output parametrised file(s). If there is more than one file, the first one is always the topology file and
+        the second one - the coordinate file.
+    """
     if id is None: id = molecule_type
     files = []
 
     if molecule_type == "protein":
         if params.protein_ff in _PC.AMBERPROTEINFFS:
-            files = _amber.amberWrapper(params, filename, molecule_type, id, *args, **kwargs)
+            files = amber.amberWrapper(params, filename, molecule_type, id, *args, **kwargs)
     elif molecule_type in ["ligand", "cofactor"]:
         if params.ligand_ff in _PC.AMBERLIGANDFFS:
-            files = _amber.amberWrapper(params, filename, molecule_type, id, *args, **kwargs)
+            files = amber.amberWrapper(params, filename, molecule_type, id, *args, **kwargs)
     elif molecule_type in ["water", "simple_anion", "complex_anion", "simple_cation", "complex_cation"]:
         if params.water_ff in _PC.AMBERWATERFFS:
-            files = _amber.amberWrapper(params, filename, molecule_type, id, *args, **kwargs)
+            files = amber.amberWrapper(params, filename, molecule_type, id, *args, **kwargs)
     else:
         raise ValueError("Invalid argument: %s. Argument must be one of: protein, ligand, cofactor, simple_anion, "
                          "complex_anion, simple_cation, complex_cation or water." % molecule_type)
--- ProtoCaller/Wrappers/__init__.py
+++ ProtoCaller/Wrappers/__init__.py
@@ -4,6 +4,7 @@ if _PC.BIOSIMSPACE:
     from . import BioSimSpacewrapper
 if _PC.MODELLER:
     from . import modellerwrapper
+from . import parmedwrapper
 from . import PDB2PQRwrapper
 from . import protosswrapper
 from . import rdkitwrapper
