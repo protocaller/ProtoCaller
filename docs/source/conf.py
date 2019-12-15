21888ff871e95482876ae450bf9de012a349dc03
Miroslav Suruzhon <36005076+miroslav351@users.noreply.github.com>
2019-12-15 21:12:30 +0000

    Changed conf.py so that documentation is read properly by readthedocs ***NO_CI***

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+#Location of Sphinx files
+sys.path.insert(0, os.path.abspath('./../..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -26,7 +26,7 @@ author = 'Miroslav Suruzhon'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1dev'
+release = '1.1'
 
 
 # -- General configuration ---------------------------------------------------
