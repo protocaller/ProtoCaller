2ced9ba9aaecf87f243639a6247b547339004e12
Miroslav <miroslav351@yahoo.com>
2020-12-25 12:58:39 +0000

    Replaced htmd-pdb2pqr with pdb2pqr30

--- ProtoCaller/Wrappers/pdb2pqrwrapper.py
+++ ProtoCaller/Wrappers/pdb2pqrwrapper.py
@@ -1,22 +1,16 @@
 # TODO:
 # 1. check proteins with modified AA's
 # 2. check numbering of ignored residues
-from contextlib import redirect_stdout as _redirect_stdout
-import logging as _logging
 import os as _os
-import sys as _sys
 import warnings as _warnings
 
-from pdb2pqr.main import runPDB2PQR as _runPDB2PQR
-from pdb2pqr.src.pdb import readPDB as _readPDB
-import propka.lib as _lib
-
 from ProtoCaller.IO.PDB import PDB as _PDB
+from ProtoCaller.Utils.runexternal import runExternal as _runExternal
 
 __all__ = ["pdb2pqrTransform"]
 
 
-def pdb2pqrTransform(filename, **kwargs):
+def pdb2pqrTransform(filename, pdb2pqr_executable="pdb2pqr30", **kwargs):
     """
     A thin wrapper around PDB2PQR which protonates an input PDB file.
 
@@ -24,33 +18,10 @@ def pdb2pqrTransform(filename, **kwargs):
     ----------
     filename : str
         Name of input file.
+    pdb2pqr_executable : str
+        Name or path to the pdb2pqr executable.
     kwargs:
-        Keyword arguments to be passed on to PDB2PQR. Supported arguments are:
-
-        ph:
-            The desired ph of the system (float).
-        verbose:
-            Whether all information is output to the logger.
-        ph_calc_method:
-            pKa calculation method (None, "propka","propka31","pdb2pka").
-        ph_calc_options:
-            optionParser like option object for propka30.
-        neutraln:
-            Make the N-terminus of this protein neutral
-        neutralc:
-            Make the C-terminus of this protein neutral
-        assign_only:
-            Only assign charges and radii - do not add atoms, debump, or optimize.
-        drop_water:
-            Remove water molecules from output.
-        debump:
-            When 1, debump heavy atoms (int).
-        opt:
-            When 1, run hydrogen optimization (int).
-        include_old_header:
-            Include most of the PDB header in output.
-        holdlist:
-            A list of residues not to be optimized, as [(resid, chain, icode)].
+        Keyword arguments to be passed on to PDB2PQR. All supported arguments can be found at e.g. pdb2pqr30 --help.
 
     Returns
     -------
@@ -59,36 +30,24 @@ def pdb2pqrTransform(filename, **kwargs):
     """
 
     default_kwargs = {
-        'chain': True,
-        'ff': 'amber',
-        'ffout': 'amber',
-        'verbose': True,
-        'drop_water': True,
-        'ph': 7,
-        'ph_calc_method': 'propka31',
-        'ph_calc_options': _lib.loadOptions('--quiet')[0]
+        'ff': 'AMBER',
+        'ffout': 'AMBER',
+        'drop-water': True,
+        'keep-chain': True,
+        'with-ph': 7,
+        'titration-state-method': 'propka',
+        'log-level': "INFO",
     }
 
     default_kwargs = {**default_kwargs, **kwargs}
-
-    def write(msg):
-        msg = msg.strip()
-        if msg in ["", "*"] or "100%" in msg or "progress" in msg:
-            return None
-        if msg[-1] == ":":
-            msg = msg[:-1] + "..."
-        return _logging.info(msg)
-
-    _logging.basicConfig(stream=_sys.stdout)
-    _logging.write = write
-    with _redirect_stdout(_logging):
-        pdb = _readPDB(open(filename))[0]
-        pdb = _runPDB2PQR(pdb, **default_kwargs)
-
     filename_output = _os.path.splitext(filename)[0] + "_pdb2pqr.pdb"
-    with open(filename_output, "w") as f:
-        for line in pdb['lines']:
-            f.write(line)
+    command = f"{pdb2pqr_executable} {filename} {filename_output}"
+    for k, v in default_kwargs.items():
+        if v is not False:
+            command += f" --{k}"
+        if v not in [True, False]:
+            command += f" {v}"
+    _runExternal(command, procname="PDB2PQR")
 
     return fixPdb2pqrPDB(filename_output, filename, filename_output)
 
